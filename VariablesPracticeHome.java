5b6fd80269c1321d574ed9def01145fd0a748fa9
seviltagizadee <126412191+seviltagizadee@users.noreply.github.com>
2024-04-24 22:26:07 +0400

    evisi1

---
+++ VariablesPracticeHome.java
@@ -0,0 +1,18 @@
+public class VariablesPracticeHome {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println("age : " + age);
+
+        boolean hasJob = false;
+        System.out.println("hasJob : " + age);
+
+        char studentGrade = 'C';
+        System.out.println("studentGrade : " + studentGrade);
+
+        double carPrice = 25000;
+        System.out.println("carPrice : " +carPrice);
+
+        String employeeName = "Məlik";
+        System.out.println("employeeName : " +employeeName);
+    }
+}
