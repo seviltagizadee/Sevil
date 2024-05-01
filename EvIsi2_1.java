3362f3995552269abf026764d7e50b902dd06f5a
seviltagizadee <126412191+seviltagizadee@users.noreply.github.com>
2024-05-01 23:42:42 +0400

    Add files via upload

---
+++ EvIsi2_1.java
@@ -0,0 +1,10 @@
+package EvIsi2;
+
+public class EvIsi2_1 {
+    public static void main(String[] args) {
+        for(int i=15; i<=40;i++){
+            System.out.println(i);
+        }
+    }
+
+}
