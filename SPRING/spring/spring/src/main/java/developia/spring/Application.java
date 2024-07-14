c56bbf48e43f0f7970f063abe297c27d5c8e25e2
seviltagizadee <126412191+seviltagizadee@users.noreply.github.com>
2024-07-14 16:44:36 +0400

    EvIsi1

--- SPRING/spring/spring/src/main/java/developia/spring/Application.java
+++ SPRING/spring/spring/src/main/java/developia/spring/Application.java
@@ -2,12 +2,22 @@ package developia.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+ ApplicationContext context =SpringApplication.run(Application.class, args);
+	Person p = context.getBean(Person.class);
+		System.out.println("ID:"+p.getId());
+		System.out.println("NAME:"+p.getName());
+		System.out.println("AGE:"+p.getAge());
+		System.out.println("SALARY:"+p.getSalary());
+		 String[] beans = context.getBeanDefinitionNames();
+		for (String bean: beans) {
+			System.out.println(bean);
+		}
 	}
 
 }
