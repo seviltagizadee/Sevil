cd7a822ab124231ec1c44124ce1651759741ac3a
seviltagizadee <126412191+seviltagizadee@users.noreply.github.com>
2024-07-25 20:14:29 +0400

    evisi

--- SPRING/spring/spring/src/main/java/developia/spring/controller/CarController.java
+++ SPRING/spring/spring/src/main/java/developia/spring/controller/CarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
@@ -15,7 +16,7 @@ public class CarController {
     @Autowired
     private CarRepo repo;
     @GetMapping("/save")
-    public void saveCar(){
+    public List<Car> saveCar(){
         LocalDate localDateBmw =LocalDate.of(2023,5,25);
         Car car1 = new Car("BMW",100_000,localDateBmw);
         repo.save(car1);
@@ -27,5 +28,7 @@ public class CarController {
         LocalDate localDateMercedes =LocalDate.of(2024,2,20);
         Car car3 = new Car("Mercedes",30_000,localDateMercedes);
         repo.save(car3);
+
+        return repo.findAll();
     }
 }
